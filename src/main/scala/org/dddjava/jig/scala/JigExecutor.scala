c3de33deea73248387fa7a0b512b2bfd9e32a774
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2021-06-14 10:31:20 +0900

    update to jig-core 2021.6.2

--- src/main/scala/org/dddjava/jig/scala/JigExecutor.scala
+++ src/main/scala/org/dddjava/jig/scala/JigExecutor.scala
@@ -1,7 +1,5 @@
 package org.dddjava.jig.scala
 
-import org.dddjava.jig.infrastructure.resourcebundle.Utf8ResourceBundle
-
 import java.nio.file.Path
 import scala.collection.JavaConverters._
 
@@ -9,7 +7,6 @@ object JigExecutor {
 
   def jigReports(cliConfig: JigConfig): Unit = {
 
-    val jigMessages   = Utf8ResourceBundle.messageBundle()
     val configuration = cliConfig.configuration()
 
     println(
@@ -25,20 +22,15 @@ object JigExecutor {
     val readStatuses = jigSourceReadService.readSourceFromPaths(sourcePaths)
 
     if (readStatuses.hasError) {
-      println(jigMessages.getString("failure"))
       readStatuses.listErrors().asScala.foreach { readStatus =>
-        println(jigMessages.getString("failure.details"), jigMessages.getString(readStatus.messageKey))
+        println(s"${readStatus.localizedMessage()}")
       }
       throw new RuntimeException("failure")
     }
 
     if (readStatuses.hasWarning) {
-      println(jigMessages.getString("implementation.warning"))
       readStatuses.listWarning().asScala.foreach { readStatus =>
-        println(
-          jigMessages.getString("implementation.warning.details"),
-          jigMessages.getString(readStatus.messageKey)
-        )
+        println(s"${readStatus.localizedMessage()}")
       }
     }
 
@@ -50,9 +42,8 @@ object JigExecutor {
       .map { handleResult => handleResult.jigDocument + " : " + handleResult.outputFilePathsText() }.mkString("\n")
 
     println(
-      s"-- output documents -------------------------------------------\n${resultLog}\n------------------------------------------------------------"
+      s"-- Output Complete ${System.currentTimeMillis - startTime} ms -------------------------------------------\n${resultLog}\n------------------------------------------------------------"
     )
-    println(jigMessages.getString("success"), System.currentTimeMillis - startTime)
   }
 
 }
